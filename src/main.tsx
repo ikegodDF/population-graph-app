f150233681d390a4fef6b566ee24b6d1e045966b
いけがみ <takahashi.ryosuke.t6@elms.hokudai.ac.jp>
2025-08-23 18:34:14 +0900

    main.tsxの修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Page from "./pages/page.tsx";
+import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Page />
+    <App />
   </StrictMode>
 );
